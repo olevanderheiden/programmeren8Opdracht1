83c82bdf601a602241b68fdfcbc72bbf2d01ad02
olevanderheiden <olevanderheiden@hotmail.com>
2025-02-01 13:13:29 +0100

    V5.0 Updating Readme and adding more prevention of questions being asked multiple times in the promtps send to the game.

--- client/script.js
+++ client/script.js
@@ -81,7 +81,9 @@ const generateQuiz = async (
     request = `You are ${character} from the The Legend of Heroes: Trails series made by Nihon Falcom. Introduce yourself in less than 100 characters and then host a quiz in character with a question and four possible answers. Indicate which answer is correct. Limit your self to only questions about the Trails series. And only ask questions that ${character} would know about.`;
   } else {
     //If the character has introduced themselves, generate a request to host a quiz question without introducing the character
-    request = `You are ${character} from the The Legend of Heroes: Trails series made by Nihon Falcom. Host a quiz in character with a question and four possible answers. Indicate which answer is correct. Limit your self to only questions about the Trails series. And only ask questions that ${character} would know about.`;
+    request = `You are ${character} from the The Legend of Heroes: Trails series made by Nihon Falcom. Host a quiz in character with a question and four possible answers. Indicate which answer is correct. Limit your self to only questions about the Trails series. And only ask questions that ${character} would know about. You asked the following questions allready ${[
+      ...askedQuestions,
+    ].join(", ")}.`;
     if (previousAnswer) {
       // If the player has answered a question bfore, generate a request to provide feedback based on the player's answer and score
       request += ` Comment on the player's performance based on their score and previous answer. Dot this in character. The previous answer was ${previousAnswer}. And the total score the player currently has is ${score}. The previous question was ${previousQuestion}. Do this in character too! Avoid reintroducing yourself!`;
@@ -98,9 +100,7 @@ const generateQuiz = async (
 
     //Send a request to the server to generate a quiz question
     const response = await fetch(
-      `https://didactic-trout-wgrvvqqpwj6c75w-3000.app.github.dev/quiz?request=${encodeURIComponent(
-        request
-      )}`
+      `http://localhost:3000/quiz?request=${encodeURIComponent(request)}`
     );
     // Parse the JSON response and hide the thinking indicator
     const data = await response.json();
